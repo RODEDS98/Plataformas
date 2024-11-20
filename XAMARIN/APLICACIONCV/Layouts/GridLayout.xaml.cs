eb7a8734c9796984661eabe90d385662064e22c5
Edgard Rodriguez <edgard.rodriguez@ucsp.edu.pe>
2024-11-20 16:10:14 -0500

    Add files via upload

---
+++ XAMARIN/APLICACIONCV/Layouts/GridLayout.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+
+namespace APLICACIONCV.Layouts
+{
+    public partial class GridLayout : ContentPage
+    {
+        public GridLayout()
+        {
+            InitializeComponent();
+        }
+    }
+}
