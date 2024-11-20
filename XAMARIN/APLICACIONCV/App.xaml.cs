4d44eefd6ab27b09b852fa5ea19dbdf366d59a9c
Edgard Rodriguez <edgard.rodriguez@ucsp.edu.pe>
2024-11-20 15:58:57 -0500

    Add files via upload

---
+++ XAMARIN/APLICACIONCV/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace APLICACIONCV
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            // Establecer la página principal como una NavigationPage
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
